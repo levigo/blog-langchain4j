70021b7926d59453c9ad36b4b6be4c1e0330c617
David Hoffmann <D.Hoffmann@levigo.de>
2024-09-09 09:13:05 +0200

    update to langchain4j 0.34.0

--- src/test/java/com/jadice/blog/l4j/FunctionCallingTest.java
+++ src/test/java/com/jadice/blog/l4j/FunctionCallingTest.java
@@ -18,127 +18,122 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Function calling tests. We provide some methods to compute math and a
- * "kwigglydiggly" value for a number and see if the LLM calls the functions.
+ * Function calling tests. We provide some methods to compute math and a "kwigglydiggly" value for a
+ * number and see if the LLM calls the functions.
  * <p>
  * 
- * <b>NOTE</b><br>
- * You need langchain4j >= 0.34.0 for this test to work. As time of writing it
- * is not released yet - in this case the current langchain4j-project can be
- * checked out locally for the 0.34.0-SNAPSHOT.
- * <p>
- * https://github.com/langchain4j/langchain4j/issues/1525
  */
 @Slf4j
-@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@SpringBootTest(
+    webEnvironment = WebEnvironment.NONE)
 public class FunctionCallingTest {
 
-	@Value("${ollama.url}")
-	private String ollamaUrl;
-	@Value("${ollama.model.instruct}")
-	private String modelName;
+  @Value("${ollama.url}")
+  private String ollamaUrl;
+  @Value("${ollama.model.instruct}")
+  private String modelName;
 
-	/**
-	 * A simple assistant with no funky stuff like memory and the like. Only a chat
-	 * method to get a response for a user message.
-	 */
-	interface ChatBot {
-		String chat(@UserMessage String message);
-	}
+  /**
+   * A simple assistant with no funky stuff like memory and the like. Only a chat method to get a
+   * response for a user message.
+   */
+  interface ChatBot {
+    String chat(@UserMessage String message);
+  }
 
-	/**
-	 * Simple Tools for this test. Each @Tool method will also set a boolean whether
-	 * it was called. Can be used in tests to check if a tool was used.
-	 */
-	@Getter
-	class Tools {
-		boolean kwigglydigglyCalled = false;
-		boolean addCalled = false;
-		boolean multiplyCalled = false;
+  /**
+   * Simple Tools for this test. Each @Tool method will also set a boolean whether it was called.
+   * Can be used in tests to check if a tool was used.
+   */
+  @Getter
+  class Tools {
+    boolean kwigglydigglyCalled = false;
+    boolean addCalled = false;
+    boolean multiplyCalled = false;
 
-		@Tool("Adds two values, returning the sum")
-		int add(int a, int b) {
-			addCalled = true;
-			logger.info("# Calling tool function add() with parameters {} + {}", a, b);
-			return a + b;
-		}
+    @Tool("Adds two values, returning the sum")
+    int add(int a, int b) {
+      addCalled = true;
+      logger.info("# Calling tool function add() with parameters {} + {}", a, b);
+      return a + b;
+    }
 
-		@Tool("Multiply two values")
-		int multiply(int a, int b) {
-			multiplyCalled = true;
-			logger.info("# Calling tool function multiply() with parameters {} * {}", a, b);
-			return a * b;
-		}
+    @Tool("Multiply two values")
+    int multiply(int a, int b) {
+      multiplyCalled = true;
+      logger.info("# Calling tool function multiply() with parameters {} * {}", a, b);
+      return a * b;
+    }
 
-		@Tool("Compute the kwigglydiggly value of a number")
-		int computeKwigglydiggly(double a) {
-			kwigglydigglyCalled = true;
-			// In the end, the "kwigglydiggly" value is the original value * 42; rounded to
-			// int
-			logger.info("# Calling tool function computeKwigglydiggly() with parameters {}", a);
-			return ((Long) Math.round(a * 42d)).intValue();
-		}
+    @Tool("Compute the kwigglydiggly value of a number")
+    int computeKwigglydiggly(double a) {
+      kwigglydigglyCalled = true;
+      // In the end, the "kwigglydiggly" value is the original value * 42; rounded to
+      // int
+      logger.info("# Calling tool function computeKwigglydiggly() with parameters {}", a);
+      return ((Long) Math.round(a * 42d)).intValue();
+    }
 
-	}
+  }
 
-	@Test
-	public void testThat_functionCallingKwigglyDigglySimpleWorks() {
-		logger.info("----- testThat_functionCallingKwigglyDigglySimpleWorks");
+  @Test
+  public void testThat_functionCallingKwigglyDigglySimpleWorks() {
+    logger.info("----- testThat_functionCallingKwigglyDigglySimpleWorks");
 
-		OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaUrl).modelName(modelName)
-				.timeout(Duration.ofMinutes(5)).temperature(0.0).build();
+    OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaUrl).modelName(modelName).timeout(
+        Duration.ofMinutes(5)).temperature(0.0).build();
 
-		Tools tools = new Tools();
+    Tools tools = new Tools();
 
-		ChatBot assistant = AiServices.builder(ChatBot.class).chatLanguageModel(model).tools(tools).build();
-		String answer = assistant.chat("What is the kwigglydiggly value of pi?"); // (pi * 42) = 131.94...
+    ChatBot assistant = AiServices.builder(ChatBot.class).chatLanguageModel(model).tools(tools).build();
+    String answer = assistant.chat("What is the kwigglydiggly value of pi?"); // (pi * 42) =
+                                                                              // 131.94...
 
-		logger.info(answer);
+    logger.info(answer);
 
-		assertTrue(answer.toLowerCase().contains("132"));
-		assertTrue(tools.isKwigglydigglyCalled());
-	}
+    assertTrue(answer.toLowerCase().contains("132"));
+    assertTrue(tools.isKwigglydigglyCalled());
+  }
 
-	@Test
-	public void testThat_functionCallingKwigglyDigglyComplexNotWorks() {
-		logger.info("----- testThat_functionCallingKwigglyDigglyComplexNotWorks");
+  @Test
+  public void testThat_functionCallingKwigglyDigglyComplexNotWorks() {
+    logger.info("----- testThat_functionCallingKwigglyDigglyComplexNotWorks");
 
-		OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaUrl).modelName(modelName)
-				.timeout(Duration.ofMinutes(5)).temperature(0.0).build();
+    OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaUrl).modelName(modelName).timeout(
+        Duration.ofMinutes(5)).temperature(0.0).build();
 
-		ChatBot assistant = AiServices.builder(ChatBot.class).chatLanguageModel(model).tools(new Tools()).build();
-		try {
-			String answer = assistant.chat("Compute the kwigglydiggly value of pi and multiply by 2.0");
+    ChatBot assistant = AiServices.builder(ChatBot.class).chatLanguageModel(model).tools(new Tools()).build();
+    try {
+      String answer = assistant.chat("Compute the kwigglydiggly value of 5 and multiply by 2.0");
 
-			logger.info(answer);
+      logger.info(answer);
 
-			// does not work usually...
-			assertFalse(answer.toLowerCase().contains("264"),
-					"Wow, they fixed it. Assumed not to get the right result");
-		} catch (Exception e) {
-			// yep
-			logger.info("Exception as expected ({})", e.getMessage());
-		}
-	}
+      // does not work usually...
+      assertFalse(answer.toLowerCase().contains("264"), "Wow, they fixed it. Assumed not to get the right result");
+    } catch (Exception e) {
+      // yep
+      logger.info("Exception as expected ({})", e.getMessage());
+    }
+  }
 
-	@Test
-	public void testThat_functionCallingMathWorks() {
-		logger.info("----- testThat_functionCallingMathWorks");
+  @Test
+  public void testThat_functionCallingMathWorks() {
+    logger.info("----- testThat_functionCallingMathWorks");
 
-		OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaUrl).modelName(modelName)
-				.timeout(Duration.ofMinutes(5)).temperature(0.0).build();
+    OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaUrl).modelName(modelName).timeout(
+        Duration.ofMinutes(5)).temperature(0.0).build();
 
-		Tools tools = new Tools();
+    Tools tools = new Tools();
 
-		ChatBot assistant = AiServices.builder(ChatBot.class).chatLanguageModel(model).tools(tools).build();
-		String answer = assistant.chat("What is 1+2 and 3*4?");
+    ChatBot assistant = AiServices.builder(ChatBot.class).chatLanguageModel(model).tools(tools).build();
+    String answer = assistant.chat("What is 1+2 and 3*4?");
 
-		logger.info(answer);
+    logger.info(answer);
 
-		assertTrue(answer.toLowerCase().contains("3"));
-		assertTrue(answer.toLowerCase().contains("12"));
-		assertTrue(tools.isAddCalled());
-		assertTrue(tools.isMultiplyCalled());
-		assertFalse(tools.isKwigglydigglyCalled());
-	}
+    assertTrue(answer.toLowerCase().contains("3"));
+    assertTrue(answer.toLowerCase().contains("12"));
+    assertTrue(tools.isAddCalled());
+    assertTrue(tools.isMultiplyCalled());
+    assertFalse(tools.isKwigglydigglyCalled());
+  }
 }
--- src/test/java/com/jadice/blog/l4j/ImageRecognitionTest.java
+++ src/test/java/com/jadice/blog/l4j/ImageRecognitionTest.java
@@ -13,44 +13,45 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.ImageContent;
 import dev.langchain4j.data.message.TextContent;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.ollama.OllamaChatModel;
 import dev.langchain4j.model.output.Response;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * Image recognition tests using the "llava" model
- * (https://ollama.com/library/llava).
+ * Image recognition tests using the "llava" model (https://ollama.com/library/llava).
  */
 @Slf4j
-@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+@SpringBootTest(
+    webEnvironment = WebEnvironment.NONE)
 public class ImageRecognitionTest {
 
-	@Value("${ollama.url}")
-	private String ollamaUrl;
-	@Value("${ollama.model.image:llava}")
-	private String modelName;
+  @Value("${ollama.url}")
+  private String ollamaUrl;
+  @Value("${ollama.model.image:llava}")
+  private String modelName;
 
-	@Test
-	public void testThat_imageRecognitionWorks() {
-		logger.info("----- testThat_imageRecognitionWorks");
+  @Test
+  public void testThat_imageRecognitionWorks() {
+    logger.info("----- testThat_imageRecognitionWorks");
 
-		OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaUrl).modelName(modelName)
-				.timeout(Duration.ofMinutes(5)).temperature(0.0).build();
+    String ollamaUrl = "http://localhost:11434";
+    String modelName = "llava";
 
-		String prompt = "What do you see?";
+    OllamaChatModel model = OllamaChatModel.builder().baseUrl(ollamaUrl).modelName(modelName).timeout(
+        Duration.ofMinutes(5)).temperature(0.0).build();
 
-		dev.langchain4j.data.message.UserMessage userMessage = dev.langchain4j.data.message.UserMessage
-				.from(TextContent.from(prompt), ImageContent.from(
-						new File(System.getProperty("user.dir"), "/src/test/resources/images/parrot.jpg").toURI()));
+    String prompt = "What do you see?";
 
-		logger.debug("Sending image request...");
-		Response<AiMessage> response = model.generate(userMessage);
+    UserMessage userMessage = UserMessage.from(TextContent.from(prompt),
+        ImageContent.from(new File(System.getProperty("user.dir"), "/src/test/resources/images/parrot.jpg").toURI()));
 
-		String answer = response.content().text();
+    logger.debug("Sending image request...");
+    Response<AiMessage> response = model.generate(userMessage);
 
-		logger.info("Image recognition: {}", answer);
+    String answer = response.content().text();
 
-		assertTrue(answer.toLowerCase().contains("bird") || answer.toLowerCase().contains("parrot"),
-				"Bird/parrot not recognized");
-	}
+    assertTrue(answer.toLowerCase().contains("bird") || answer.toLowerCase().contains("parrot"),
+        "Bird/parrot not recognized");
+  }
 }
